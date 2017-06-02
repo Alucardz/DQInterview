5b0699636244efa6cc28c19d397b54116201ec5c
Alucardz <alucardzbelmont@gmail.com>
2017-06-02 18:05:57 +0300

    bug

--- src/models/interview.ts
+++ src/models/interview.ts
@@ -1,3 +1,4 @@
+import { Question } from './question';
 export class Interview {
 
     
@@ -8,5 +9,40 @@ export class Interview {
     public set Id(v : string) {
         this._Id = v;
     }
+
     
+    private _answers : AnsweredQuestion[]     ;
+    public get Answers() : AnsweredQuestion[]      {
+        return this._answers;
+    }
+    public set Answers(v : AnsweredQuestion[]     ) {
+        this._answers = v;
+    }
+    
+    
+}
+
+class AnsweredQuestion extends Question {
+    constructor(parameters) {
+        super(parameters);
+    }
+        
+    private _answerIndex : number;
+    public get AnswerIndex() : number {
+        return this._answerIndex;
+    }
+    public set AnswerIndex(v : number) {
+        this._answerIndex = v;
+    }
+    
+       
+    private _comment : string;
+    public get Comment() : string {
+        return this._comment;
+    }
+    public set Comment(v : string) {
+        this._comment = v;
+    }
+    
+
 }
