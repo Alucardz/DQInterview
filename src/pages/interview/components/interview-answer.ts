03561736fdc65ffdf29e4086fb87846ddd170170
Alucardz <alucardzbelmont@gmail.com>
2017-06-11 10:30:05 +0300

    changed to textarea + focus bug fix

--- src/pages/interview/components/interview-answer.ts
+++ src/pages/interview/components/interview-answer.ts
@@ -1,6 +1,5 @@
-import { TextInput } from 'ionic-angular';
 import { Answer } from './../../../models/answer';
-import { Component, Input, Output, EventEmitter, ViewChild, ElementRef, ViewChildren, QueryList, ViewContainerRef } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ElementRef, ViewChildren, QueryList } from '@angular/core';
 
 /**
  * Generated class for the InterviewAnswerComponent component.
@@ -11,13 +10,13 @@ import { Component, Input, Output, EventEmitter, ViewChild, ElementRef, ViewChil
 @Component({
   selector: 'interview-answer',
   template: `
-    <button ion-item (click)="toggleComment()">
+    <button ion-item (click)="toggleComment(comment)">
       <ion-icon name="cart" item-left ></ion-icon>
         {{answerText}}
-      </button>
-      <ion-item *ngIf="answerIndex == selectedAnswer?.AnswerIndex">
-        <ion-input #comment type="text" (ngModelChange)="selectedAnswer.Comment = $event" [ngModel]="selectedAnswer?.Comment"></ion-input>
-      </ion-item>
+    </button>
+    <ion-item *ngIf="answerIndex == selectedAnswer?.AnswerIndex" >
+      <ion-textarea #comment type="text" (ngModelChange)="selectedAnswer.Comment = $event" [ngModel]="selectedAnswer?.Comment" clearInput></ion-textarea>
+    </ion-item>
   `
 })
 export class InterviewAnswerComponent {
@@ -28,10 +27,11 @@ export class InterviewAnswerComponent {
 
   Txt_Comment: ElementRef;
 
+  //
   @ViewChildren('comment', { read: ElementRef }) set content(comments: QueryList<ElementRef>) {
-    debugger;
-    if (comments.length > 0) {      
-      comments.first.nativeElement.querySelector('input').focus();      
+    if (comments.length > 0) {
+      this.Txt_Comment = comments.first;
+      this.Txt_Comment.nativeElement.querySelector('textarea').focus();
     }
   }
 
@@ -39,15 +39,15 @@ export class InterviewAnswerComponent {
 
   }
 
-  ngAfterViewInit() {
-
-  }
-
-  toggleComment() {
+  toggleComment(commentInput: ElementRef) {
     if (this.selectedAnswer.AnswerIndex != this.answerIndex) {
       this.selectedAnswer.Comment = "";
       this.selectedAnswer.AnswerIndex = this.answerIndex;
-      //this.Txt_Comment[this.selectedAnswer.AnswerIndex].nativeElement.focus()
+    }
+    else {
+      if (this.Txt_Comment) {
+        this.Txt_Comment.nativeElement.querySelector('textarea').focus();
+      }
     }
   }
 }
